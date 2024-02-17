1bffdd9dd20738c83a25c54182b0db6d27d77151
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2024-02-18 00:37:00 +0630

    Added three example places to the list

---
+++ script.js
@@ -0,0 +1,23 @@
+const travelList = [
+  {
+    city: "Barcelona",
+    country: "Spain",
+    region: "Europe",
+    haveTravelled: true
+  },
+
+  {
+    city: "Istanbul",
+    country: "Turkey",
+    region: "Eurasia",
+    haveTravelled: false
+  },
+
+  {
+    city: "Sydney",
+    country: "Australia",
+    region: "Australia",
+    haveTravelled: true
+  }
+];
+
