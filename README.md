a28d0dd111ffd22f5add5d43b60cbc02369fc8be
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2024-02-19 01:12:51 +0630

    Added DOM elements to display each place as a card

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wanderlist
+
+Image by <a href="https://pixabay.com/users/monikap-2515080/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1620398">Monika</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1620398">Pixabay</a>
+
+<a href="https://www.flaticon.com/free-animated-icons/map-pointer" title="map-pointer animated icons">Map-pointer animated icons created by Freepik - Flaticon</a>
--- script.js
+++ script.js
@@ -21,3 +21,36 @@ const travelList = [
   }
 ];
 
+//Select the grid div
+const placesGrid = document.querySelector('.places-grid');
+
+//Creating cards (Inside each card --> banner image, city name, country name, region name, travel status icon, button a user can toggle to change the travel status, remove button)
+const placeCard = document.createElement('div');
+placeCard.className = 'place-card';
+
+//Creating div for the the banner image, select the image url and give it a class name
+const cardImage = document.createElement('img');
+cardImage.src = './images/card-image.jpg'
+cardImage.className = 'card-image';
+
+//Creaing div for the city name, country name and region name and give them a class name
+const cityName = document.createElement('div');
+cityName.className = 'city-name';
+const countryName = document.createElement('div');
+countryName.className = 'country-name';
+const regionName = document.createElement('div');
+regionName.className = 'region-name';
+
+//Creating div for the action buttons (Inside the div --> travel status icon, travel status button, remove button)
+const actionButtons = document.createElement('div');
+actionButtons.className = 'action-buttons';
+
+// Creating div, button for the travel status icon, status button, remove button and give them a class name
+const travelStatusIcon = document.createElement('div');
+travelStatusIcon.className = 'travel-status-icon';
+
+const travelStatusButton = document.createElement('button');
+travelStatusButton.className = 'travel-status-button';
+
+const removeButton = document.createElement('button');
+removeButton.className = 'remove-button';
