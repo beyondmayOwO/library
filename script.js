9b3f957055542c5262f99caf073a107794bc4f4b
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2024-02-20 00:18:40 +0630

    Function that loops the list and display each place

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const travelList = [
     region: "Europe",
     haveTravelled: true
   },
-
   {
     city: "Istanbul",
     country: "Turkey",
     region: "Eurasia",
     haveTravelled: false
   },
-
   {
     city: "Sydney",
     country: "Australia",
@@ -24,33 +22,60 @@ const travelList = [
 //Select the grid div
 const placesGrid = document.querySelector('.places-grid');
 
-//Creating cards (Inside each card --> banner image, city name, country name, region name, travel status icon, button a user can toggle to change the travel status, remove button)
-const placeCard = document.createElement('div');
-placeCard.className = 'place-card';
+//Creating card for each place: loop each object in the array, create DOM elements, put the values in corresponding DOM elements
+function createCard() {
+  travelList.forEach(place => {
+    //Inside each card div --> banner image, city name, country name, region name, travel status icon, status button, remove button
+    const placeCard = document.createElement('div');
+    placeCard.className = 'place-card';
 
-//Creating div for the the banner image, select the image url and give it a class name
-const cardImage = document.createElement('img');
-cardImage.src = './images/card-image.jpg'
-cardImage.className = 'card-image';
+    //Banner image
+    const cardImage = document.createElement('img');
+    cardImage.className = 'card-image';
+    cardImage.src = './images/card-image.jpg';
 
-//Creaing div for the city name, country name and region name and give them a class name
-const cityName = document.createElement('div');
-cityName.className = 'city-name';
-const countryName = document.createElement('div');
-countryName.className = 'country-name';
-const regionName = document.createElement('div');
-regionName.className = 'region-name';
+    //City name, country name, region name
+    const cityName = document.createElement('div');
+    cityName.className = 'city-name';
+    cityName.innerText = place.city;
 
-//Creating div for the action buttons (Inside the div --> travel status icon, travel status button, remove button)
-const actionButtons = document.createElement('div');
-actionButtons.className = 'action-buttons';
+    const countryName = document.createElement('div');
+    countryName.className = 'country-name';
+    countryName.innerText = place.country;
 
-// Creating div, button for the travel status icon, status button, remove button and give them a class name
-const travelStatusIcon = document.createElement('div');
-travelStatusIcon.className = 'travel-status-icon';
+    const regionName = document.createElement('div');
+    regionName.className = 'region-name';
+    regionName.innerText = place.region;
 
-const travelStatusButton = document.createElement('button');
-travelStatusButton.className = 'travel-status-button';
 
-const removeButton = document.createElement('button');
-removeButton.className = 'remove-button';
+    //Action buttons (Inside the div --> travel status icon, travel status button, remove button)
+    const actionButtons = document.createElement('div');
+    actionButtons.className = 'action-buttons';
+
+    //Travel status icon, status button, remove button
+    const travelStatusIcon = document.createElement('img');
+    travelStatusIcon.className = 'travel-status-icon';
+
+    const travelStatusButton = document.createElement('button');
+    travelStatusButton.className = 'travel-status-button';
+
+    const removeButton = document.createElement('button');
+    removeButton.className = 'remove-button';
+
+    if(place.haveTravelled) {
+      travelStatusIcon.src = './images/have-travelled.gif';
+      travelStatusButton.innerText = 'Mark as plan to travel';
+    }
+    else {
+      travelStatusIcon.src = './images/plan-to-travel.gif';
+      travelStatusButton.innerText = 'Mark as travelled';
+    }
+
+    placesGrid.appendChild(placeCard);
+    placeCard.append(cardImage, cityName, countryName, regionName, actionButtons);
+    actionButtons.append(travelStatusIcon, travelStatusButton, removeButton);
+    }
+  )
+}
+
+createCard();
